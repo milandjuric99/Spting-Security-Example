9c379a2deff58181c839d02581a8bf915f5737ff
milandjuric99 <milan.djuric.019@gmail.com>
2021-03-11 12:19:19 +0100

    Basic Spring Security implementation

---
+++ src/main/java/com/example/securingwebj11/SecuringWebJ11Application.java
@@ -0,0 +1,13 @@
+package com.example.securingwebj11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringWebJ11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuringWebJ11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/securingwebj11/SecuringWebJ11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securingwebj11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringWebJ11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
